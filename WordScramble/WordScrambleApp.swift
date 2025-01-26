e1968fa09c81b4884749225455c6687839fa6bdb
Stefan Olarescu <stefanolarescu@gmail.com>
2025-01-26 22:38:02 +0200

    Created Xcode project.

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Stefan Olarescu on 26.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
